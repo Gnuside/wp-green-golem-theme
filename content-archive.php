494b582c8aea4f955da4fbd34e9ad81183407186
Glenn Y. Rolland <glenux@gmail.com>
2012-06-18 20:24:00 +0200

    Updated page list.

--- content-archive.php
+++ content-archive.php
@@ -3,9 +3,9 @@
 	<!-- / article title --> 
 	<header> 
 	<h2 class='title'><?php the_title(); ?></h2> 
-	 <span class='category'>Paris</span> 
-	 <span class='creation-date'>Samedi 19 mai 2012</span> 
-	 <span class='author'>Auteur</span> 
+	 <span class='category'><?php the_category(',','single'); ?></span> 
+	 <span class='creation-date'><?php the_date(); ?></span> 
+	 <span class='author'><?php the_author_posts_link(); ?></span> 
 	</header> 
 	<section>
 	</section>
