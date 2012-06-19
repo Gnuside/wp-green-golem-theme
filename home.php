eaebac8eb0984a45ba86d8ef8eb362dcfbb439c2
Glenn Y. Rolland <glenux@gmail.com>
2012-06-19 16:53:36 +0200

    Added home page.

---
+++ home.php
@@ -0,0 +1,210 @@
+<?php get_header(); ?>
+
+<section class='span-17 extended last'>
+  <header class='heading yellow'>
+	<h2>À la une</h2>
+	<a class='feed' href='#'>
+	  <img src='images/rss-black.png' />
+	</a>
+  </header>
+  <section>
+	<article class='span-11 append-1 emphasis first'>
+	  <img src='images/placeholder_mib.png' width='100%' />
+	  <header>
+		<h3 class='context'>Ils reviennent du futur pour nous remercier d'avoir lancé le revenu de base</h3>
+	  </header>
+	  <section>
+		<p>
+		  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pharetra metus a orci ultrices consectetur. Praesent nec rhoncus orci. Sed.
+		</p>
+	  </section>
+	</article>
+	<section class='span-5 extended last'>
+	  <article class='emphasis'>
+		<p class='context'>Mercredi 25 avril</p>
+		<header>
+		  <h3>Lorem ipsum</h3>
+		</header>
+		<aside class='right'>
+		  <img src='images/medium.png' />
+		</aside>
+		<section>
+		  <p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pharetra metus a orci ultrices consectetur.
+		  </p>
+		</section>
+	  </article>
+	  <article class='emphasis'>
+		<p class='context'>Mercredi 25 avril</p>
+		<header>
+		  <h3>Lorem ipsum</h3>
+		</header>
+		<section>
+		  <p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+		  </p>
+		</section>
+	  </article>
+	</section>
+  </section>
+</section>
+<div class='prepend-top span-17 extended last'>
+  <section class='span-5 extended halfcolborder'>
+	<header class='heading yellow'>
+	  <h2>Actualités</h2>
+	  <a class='feed' href='#'>
+		<img src='images/rss-black.png' />
+	  </a>
+	</header>
+	<article class='emphasis'>
+	  <p class='context'>Mercredi 25 avril</p>
+	  <header>
+		<h3>Lorem ipsum</h3>
+	  </header>
+	  <aside class='right'>
+		<img src='images/medium.png' />
+	  </aside>
+	  <section>
+		<p>
+		  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pharetra metus a orci ultrices consectetur. Praesent nec rhoncus orci. Sed.
+		</p>
+	  </section>
+	</article>
+	<article class='emphasis'>
+	  <p class='context'>Mercredi 25 avril</p>
+	  <header>
+		<h3>Lorem ipsum</h3>
+	  </header>
+	  <section>
+		<p>
+		  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pharetra metus a orci ultrices consectetur. Praesent nec rhoncus orci. Sed.
+		</p>
+	  </section>
+	</article>
+	<article class='emphasis'>
+	  <p class='context'>Mercredi 25 avril</p>
+	  <header>
+		<h3>Lorem ipsum</h3>
+	  </header>
+	  <aside class='right'>
+		<img src='images/medium.png' />
+	  </aside>
+	  <section>
+		<p>
+		  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pharetra metus a orci ultrices consectetur. Praesent nec rhoncus orci. Sed.
+		</p>
+	  </section>
+	</article>
+	<div class='prepend-top right'>
+	  <p>
+		<a href='#'>En savoir plus…</a>
+	  </p>
+	</div>
+  </section>
+  <section class='span-5 extended halfcolborder'>
+	<header class='heading orange'>
+	  <h2>Opinions</h2>
+	  <a class='feed' href='#'>
+		<img src='images/rss-black.png' />
+	  </a>
+	</header>
+	<article class='emphasis'>
+	  <p class='context'>Mercredi 25 avril</p>
+	  <header>
+		<h3>Lorem ipsum</h3>
+	  </header>
+	  <aside class='right'>
+		<img src='images/medium.png' />
+	  </aside>
+	  <section>
+		<p>
+		  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pharetra metus a orci ultrices consectetur. Praesent nec rhoncus orci. Sed.
+		</p>
+	  </section>
+	</article>
+	<article class='emphasis'>
+	  <p class='context'>Mercredi 25 avril</p>
+	  <header>
+		<h3>Lorem ipsum</h3>
+	  </header>
+	  <section>
+		<p>
+		  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pharetra metus a orci ultrices consectetur. Praesent nec rhoncus orci. Sed.
+		</p>
+	  </section>
+	</article>
+	<article class='emphasis'>
+	  <p class='context'>Mercredi 25 avril</p>
+	  <header>
+		<h3>Lorem ipsum</h3>
+	  </header>
+	  <aside class='right'>
+		<img src='images/medium.png' />
+	  </aside>
+	  <section>
+		<p>
+		  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pharetra metus a orci ultrices consectetur. Praesent nec rhoncus orci. Sed.
+		</p>
+	  </section>
+	</article>
+	<div class='prepend-top right'>
+	  <p>
+		<a href='#'>En savoir plus…</a>
+	  </p>
+	</div>
+  </section>
+  <section class='span-5 extended last'>
+	<header class='heading red'>
+	  <h2>Évènements</h2>
+	  <a class='feed' href='#'>
+		<img src='images/rss-black.png' />
+	  </a>
+	</header>
+	<article class='emphasis'>
+	  <p class='context'>Mercredi 25 avril</p>
+	  <header>
+		<h3>Lorem ipsum</h3>
+	  </header>
+	  <aside class='right'>
+		<img src='images/medium.png' />
+	  </aside>
+	  <section>
+		<p>
+		  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pharetra metus a orci ultrices consectetur. Praesent nec rhoncus orci. Sed.
+		</p>
+	  </section>
+	</article>
+	<article class='emphasis'>
+	  <p class='context'>Mercredi 25 avril</p>
+	  <header>
+		<h3>Lorem ipsum</h3>
+	  </header>
+	  <section>
+		<p>
+		  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pharetra metus a orci ultrices consectetur. Praesent nec rhoncus orci. Sed.
+		</p>
+	  </section>
+	</article>
+	<article class='emphasis'>
+	  <p class='context'>Mercredi 25 avril</p>
+	  <header>
+		<h3>Lorem ipsum</h3>
+	  </header>
+	  <aside class='right'>
+		<img src='images/medium.png' />
+	  </aside>
+	  <section>
+		<p>
+		  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pharetra metus a orci ultrices consectetur. Praesent nec rhoncus orci. Sed.
+		</p>
+	  </section>
+	</article>
+	<div class='prepend-top right'>
+	  <p>
+		<a href='#'>En savoir plus…</a>
+	  </p>
+	</div>
+  </section>
+</div>
+
+<?php get_footer(); ?>
