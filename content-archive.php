4db5e4185bbe9cccb3769c00d845f9c0578f5777
Glenn Y. Rolland <glenux@gmail.com>
2012-06-19 16:53:47 +0200

    Added some other page.

--- content-archive.php
+++ content-archive.php
@@ -1,8 +1,27 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>> 
-	<img class='illustration' src='<?php echo get_template_directory_uri(); ?>/images/placeholder_mib.png' /> 
+<?php
+/* prepare content */
+$post = get_post( get_the_ID() );
+
+/* use featured image (thumbnail) if possible
+ * or placeholder instead
+ */
+$post_thumbnail_url = null;
+if ( has_post_thumbnail( $post->id )) {
+	$post_thumbnail_url = wp_get_attachment_url( get_post_thumbnail_id( $post->id ) );
+} else {
+	$post_thumbnail_url = get_template_directory_uri() . '/images/thumb_placeholder.png';
+}
+
+?>
+<article id="post-<?php echo $post->id; ?>" <?php post_class(); ?>> 
+<a href='<?php post_permalink($post->id); ?>'>
+		<img class='illustration' src='<?php echo $post_thumbnail_url; ?>' /> 
+	</a>
 	<!-- / article title --> 
 	<header> 
-	<h2 class='title'><?php the_title(); ?></h2> 
+	<h2 class='title'>
+		<a href=""><?php $post->title; ?></a>
+	</h2> 
 	 <span class='category'><?php the_category(',','single'); ?></span> 
 	 <span class='creation-date'><?php the_date(); ?></span> 
 	 <span class='author'><?php the_author_posts_link(); ?></span> 
