f52793f0adb6a89352b1bc9a1e83fc35ebc9742a
Glenn Y. Rolland <glenux@gmail.com>
2012-06-23 22:04:36 +0200

    w⁻template: Fixed post category handling.

--- category.php
+++ category.php
@@ -18,11 +18,15 @@
 if(have_posts()) {
 	while(have_posts()) { 
 		the_post(); 
-		echo get_post_format();
-		get_template_part( 'content', get_post_format() ); 
+		$format = get_post_format( $post->ID );
+		if ( "" == $format ) {
+			$format = "archive";
+		}
+		echo "content-$format<br/>";
+		get_template_part( 'content', $format ); 
 	}
 } else {
-	echo "Apologies, but no results were found for the requested archive. Perhaps searching will help find a rel  ated post.";
+	echo "Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.";
 }
 ?>
 
