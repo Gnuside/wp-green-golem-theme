4f35e685f60d9f0696521bc39de1e64df172366a
Glenn Y. Rolland <glenux@gmail.com>
2012-06-24 11:23:07 +0200

    wp-template: Updated functions comments.

--- functions.php
+++ functions.php
@@ -40,8 +40,10 @@ if ( ! function_exists( 'revenudebase_init_menus' ) ) {
 	}
 }
 
-/* Usefull functions */
 
+/* 
+ * Retrieve thumbnail URL or set default thumbnail
+ */
 function revenudebase_get_thumbnail_url( $post_id, $thumb_size = 'small' ) {
 	$post_thumbnail_url = null;
 	if ( has_post_thumbnail( $post->id )) {
@@ -51,6 +53,37 @@ function revenudebase_get_thumbnail_url( $post_id, $thumb_size = 'small' ) {
 		$post_thumbnail_url = get_template_directory_uri() . '/images/thumb_placeholder.png';
 	}
 	return $post_thumbnail_url;
+}
+
+
+/* 
+ * Return pagination links (FIXME! to be tested!)
+ */
+function revenudebase_pagination()
+{
+	global $wp_query, $wp_rewrite;
+	$wp_query->query_vars['paged'] > 1 ? $current = $wp_query->query_vars['paged'] : $current = 1;
+
+	$pagination = array(
+		'base' => @add_query_arg('page','%#%'),
+		'format' => '',
+		'total' => $wp_query->max_num_pages,
+		'current' => $current,
+		'show_all' => false,
+		'end_size'     => 1,
+		'mid_size'     => 2,
+		'type' => 'list',
+		'next_text' => '»',
+		'prev_text' => '«'
+	);
+
+	if( $wp_rewrite->using_permalinks() )
+		$pagination['base'] = user_trailingslashit( trailingslashit( remove_query_arg( 's', get_pagenum_link( 1 ) ) ) . 'page/%#%/', 'paged' );
+
+	if( !empty($wp_query->query_vars['s']) )
+		$pagination['add_args'] = array( 's' => get_query_var( 's' ) );
+
+	echo str_replace('page/1/','', paginate_links( $pagination ));
 }
 
 /**
