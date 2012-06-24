a675b4274a794ac635a292db596796a402ba6291
Glenn Y. Rolland <glenux@gmail.com>
2012-06-24 11:23:37 +0200

    wp-template: Fix category feed url.

--- category.php
+++ category.php
@@ -1,29 +1,28 @@
 <?php get_header(); ?>
 
-<section id='article-list'>                                                                                   
-
-<header class='heading yellow'>                                                                                       
-  <!-- FIXME: get search title ... -->
-  <h2><?php single_cat_title(); ?></h2>                                                                                                 
-  <!-- FIXME: verify the feed is valid for all searches -->
-  <a class='feed' href='<?php echo get_category_feed_link( ); ?>'>                                                                                           
-  <img src='<?php echo get_template_directory_uri(); ?>/images/rss-black.png' />                                                                                
-  </a>                                                                                                                
-</header> 
-
+<section id='article-list'>
+<header class='heading yellow'>
+<?php $category = get_category(get_query_var('cat'),false); ?>
+  <h2><?php echo $category->name;  ?></h2>
+  <a class='feed' href='<?php echo get_category_feed_link( 
+$category->cat_ID ); ?>'>
+  <img src='<?php echo get_template_directory_uri(); ?>/images/rss-black.png' />
+  </a>
+</header>
 
 <!--FIXME: insérer la liste des pages -->
+<?php revenudebase_pagination(); ?>
 
-<?php 
+<?php
 if(have_posts()) {
-	while(have_posts()) { 
-		the_post(); 
+	while(have_posts()) {
+		the_post();
 		$format = get_post_format( $post->ID );
 		if ( "" == $format ) {
 			$format = "archive";
 		}
 		echo "content-$format<br/>";
-		get_template_part( 'content', $format ); 
+		get_template_part( 'content', $format );
 	}
 } else {
 	echo "Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.";
@@ -32,6 +31,7 @@ if(have_posts()) {
 
 
 <!--FIXME: insérer la liste des pages -->
+<?php revenudebase_pagination(); ?>
 
 </section> <!-- #article-list -->
 
