a6a8be4c5f4256537dfe821388de439958baa439
Glenn Y. Rolland <glenux@gmail.com>
2012-07-01 14:48:49 +0200

    wp-template: Updated page template.

--- content-page.php
+++ content-page.php
@@ -1,13 +1,34 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>                                                                                                           
-	<img class='illustration' src='images/placeholder_mib.png' />                                                     
-	<!-- / article title -->                                                                                          
-	<header>                                                                                                          
-	<h2 class='title'><?php the_title(); ?></h2>                                                                                                           
-	  <span class='category'>Paris</span>                                                                             
-	  <span class='creation-date'>Samedi 19 mai  2012</span>                                                          
-	  <span class='author'>Auteur</span>                                                                              
-	</header>                                                                                                         
-	<section>                                                                                                         
-		<?php the_content(); ?>
-	</section>                                                                                                        
-</article><!-- #post-<?php the_ID(); ?> -->
+
+<article id='article-full' class="post-<?php the_ID(); ?>" <?php post_class(); ?> >
+
+  <!-- / article title -->
+  <h2>
+    <span class='title'><?php the_title(); ?></span>
+    <span class='category'>
+      Paris
+    </span>
+	 <?php if ( 'post' == get_post_type() ) : ?>
+    <span class='creation-date'>
+	<?php the_date(); ?>
+          </span><!-- .creation-date -->
+          <?php endif; ?>
+  </h2>
+  <hr style='clear:both; visibility:hidden' />
+  <img align='right' class='right last' height='200' src='http://www.mutinerie.org/wp-content/uploads/2012/02/Fernand_Leger.jpg' width='450px' />
+
+<?php the_content(); ?>
+
+  <ul class='tags'>
+	<?php the_tags('<li>','</li><li>','</li>'); ?>
+  </ul>
+  <div id='sharebox'>
+    <div class='addthis_toolbox addthis_default_style '>
+      <a class='addthis_button_facebook_like' fb:like:layout='button_count'></a>
+      <a class='addthis_button_tweet' tw:related='revenudebase' tw:via='revenudebase'></a>
+      <a class='addthis_button_google_plusone' g:plusone:size='medium'></a>
+      <!-- /%a{ :class=>"addthis_counter addthis_pill_style" } -->
+      <script src='http://s7.addthis.com/js/250/addthis_widget.js' type='text/javascript'></script>
+    </div>
+  </div>
+  <?php comments_template(); ?>
+</article>
--- page.php
+++ page.php
@@ -2,7 +2,7 @@
 
 <?php while ( have_posts() ) : the_post(); ?>
 
-	<?php get_template_part( 'content', 'single' ); ?>
+	<?php get_template_part( 'content', 'page' ); ?>
 
 <?php endwhile; // end of the loop. ?>
 
