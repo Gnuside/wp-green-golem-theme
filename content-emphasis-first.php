7ba375e6d3d606bbdf8011deb506f66f8935811b
Glenn Y. Rolland <glenux@gmail.com>
2012-07-05 15:35:55 +0200

    wp-template & css: re-harmonized font sizes accross the site.

--- content-emphasis-first.php
+++ content-emphasis-first.php
@@ -4,8 +4,8 @@
 		<a href='<?php echo post_permalink($post->ID); ?>'><?php echo get_the_post_thumbnail($post->ID, array(450,450)); ?></a>
 	</aside>
 	  <header>
-		<p class='context'><?php echo get_the_date(); ?></p>
 		<h3><a href='<?php echo post_permalink($post->ID); ?>'><?php the_title(); ?></a></h3>
+		<p class='context'><?php echo get_the_date(); ?></p>
 	  </header>
 	<section>
 		<a href='<?php echo post_permalink($post->ID); ?>'><?php the_excerpt(); ?></a>
