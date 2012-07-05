31a9d27d73ba35c9ab80466d115402c73a8e6b0f
Glenn Y. Rolland <glenux@gmail.com>
2012-07-05 18:57:31 +0200

    wp-template: update em-functions.

--- inc/em-functions.php
+++ inc/em-functions.php
@@ -2,11 +2,20 @@
 add_filter('em_event_save','revenudebase_em_categorize',1,2);
 function revenudebase_em_categorize($result,$EM_Event){
 	global $wpdb;
-	// FIXME get the revenudebase event category from options
+
+	// get the revenudebase event category from options
 	$options = get_option( 'revenudebase_options_prefix' );
-	$cat_id = $options['event_category_id'];
+
+	$cat_ids = array( $options['event_category_id'] );
+	$cat_ids = array_map('intval', $cat_ids);
+
+	//$cat_ids = array_unique( $cat_ids );
+	// print_r($EM_Event);
+	// print_r($cat_ids);
+	// die();
+	
 
 	// FIXME set the category to event	
-	wp_set_object_terms( $EM_Event->post_id, $cat_id, 'category' );
+	wp_set_object_terms( $EM_Event->post_id, $cat_ids, 'category' );
 }
 ?>
