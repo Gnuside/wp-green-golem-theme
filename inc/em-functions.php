b4c3528a93014bb1e425cbd6072e0e92346e7a1a
Glenn Y. Rolland <glenux@gmail.com>
2012-07-06 13:11:34 +0200

    wp-template: em-functions disable logging.

--- inc/em-functions.php
+++ inc/em-functions.php
@@ -10,7 +10,7 @@ function revenudebase_em_categorize($result,$EM_Event){
 	// and ONLY THEN ask for what we want
 	$options = get_option( 'revenudebase_options_prefix' );
 
-	syslog(LOG_WARNING, "Options : ".var_export($options, true));
+	// syslog(LOG_WARNING, "Options : ".var_export($options, true));
 
 	$cat_ids = array( (int)($options['event_category_id']) );
 	$cat_ids = array_unique( $cat_ids );
@@ -18,8 +18,8 @@ function revenudebase_em_categorize($result,$EM_Event){
 	// syslog(LOG_WARNING, var_export($EM_Event, true));
 
 	// FIXME set the category to event	
-	syslog(LOG_WARNING, "EM post_id : ".$EM_Event->post_id);
-	syslog(LOG_WARNING, "EM cat_ids : ".var_export($cat_ids,true));
+	// syslog(LOG_WARNING, "EM post_id : ".$EM_Event->post_id);
+	// syslog(LOG_WARNING, "EM cat_ids : ".var_export($cat_ids,true));
 
 	$res = wp_set_post_terms( $EM_Event->post_id, 
 		$cat_ids, 'category' );
@@ -32,6 +32,6 @@ function revenudebase_em_categorize($result,$EM_Event){
 	//$res = wp_get_post_terms( $EM_Event->post_id, 
 	//	'category');
 
-	syslog(LOG_WARNING, "taxonomy : ". var_export($res,true));
+	// syslog(LOG_WARNING, "taxonomy : ". var_export($res,true));
 }
 ?>
