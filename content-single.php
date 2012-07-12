0014f903a4ba79d683936c29f791bb50ef43a4b5
Glenn Y. Rolland <glenux@gmail.com>
2012-07-12 19:11:52 +0200

    Fixed anchor (once again).

--- content-single.php
+++ content-single.php
@@ -24,8 +24,8 @@
   </ul>
 
   <!-- / author box -->
-  <a name="authorbox"></a>
   <aside class='author'>
+    <a name="authorbox" class="anchor"></a>
     <div class='thumbnail right'>
 		<a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>">
 		<?php echo get_avatar( get_the_author_meta( 'ID' )); ?>
