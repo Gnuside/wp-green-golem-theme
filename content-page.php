be8f258874753f11147bbc968d5baf8288027f77
Glenn Y. Rolland <glenux@gmail.com>
2012-07-12 18:58:12 +0200

    Updted content page.

--- content-page.php
+++ content-page.php
@@ -14,19 +14,7 @@
   <hr style='clear:both; visibility:hidden' />
   <?php echo get_the_post_thumbnail($post->ID, array(450,450), array('class'=>'right last')); ?>
 
-<?php the_content(); ?>
+  <?php the_content(); ?>
 
-  <ul class='tags'>
-	<?php the_tags('<li>','</li><li>','</li>'); ?>
-  </ul>
-  <div id='sharebox'>
-    <div class='addthis_toolbox addthis_default_style '>
-      <a class='addthis_button_facebook_like' fb:like:layout='button_count'></a>
-      <a class='addthis_button_tweet' tw:related='revenudebase' tw:via='revenudebase'></a>
-      <a class='addthis_button_google_plusone' g:plusone:size='medium'></a>
-      <!-- /%a{ :class=>"addthis_counter addthis_pill_style" } -->
-      <script src='http://s7.addthis.com/js/250/addthis_widget.js' type='text/javascript'></script>
-    </div>
-  </div>
   <?php comments_template(); ?>
 </article>
